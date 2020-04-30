8c652b7177316f3ef7214b47095fc3242bcf025e
maui <maui@devf.mx>
2020-04-29 21:55:26 -0500

    Agregar middlewares para manejar el body y agregar endpoints de usuario

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 3000;
 
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
 // Endpoints
 server.get('/', (req, res) => res.send('Hello World!'));
 
@@ -11,6 +14,24 @@ server.get('/api/v1/temario/:id', (req, res) => {
   console.log(req.query);
   console.log(req.params);
   res.status(200).send("cool");
+});
+
+const Users = require('../models/Users');
+
+// CRUD Usuario
+
+// CREATE
+server.post('/api/v1/users', (req, res) => {
+  Users.create(req.body)
+    .then(user => res.status(201).send(user))
+    .catch(err => res.status(400).send({ message: 'Error creating user', err }));
+});
+
+// GET (ALL)
+server.get('/api/v1/users', (req, res) => {
+  Users.find()
+    .then(users => res.status(200).send(users))
+    .catch(err => res.status(404).send({ message: 'Users not found', err }));
 });
 
 // exportar server para poder requerirlo desde otros archivos
