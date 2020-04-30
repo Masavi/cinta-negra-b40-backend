57c3a4c2e68cd1fe2681e9070717533cc8555714
maui <maui@devf.mx>
2020-04-29 20:55:16 -0500

    Renombar app a server para volver el codigo mas legible

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 1) Obtenemos app y PORT mediante "destructuring"
 // 2) app y PORT vienen del archivo index de la carpeta server
-// NOTA: No es necesario agregar "index" cuando se solicita mediante require
-const { app, PORT } = require('./server');
+// NOTA: No es necesario agregar "index" cuando se solicita mediante 
+const { server, PORT } = require('./server');
 
 // Encedemos servidor
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
