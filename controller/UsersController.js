ce923c3a4631d89964144ae9df7019cb42d6984c
maui <maui@devf.mx>
2020-05-01 12:41:45 -0500

    Refactorizar then y catch a async/await

--- controller/UsersController.js
+++ controller/UsersController.js
@@ -1,14 +1,20 @@
 const { UsersService } = require('../services');
 
 module.exports = {
-  create: (req, res) => {
-    UsersService.create(req.body)
-      .then(user => res.status(201).send(user))
-      .catch(err => res.status(400).send({ message: 'Error creating user', err }));
+  create: async (req, res) => {
+    try {
+      const user = await UsersService.create(req.body);
+      res.status(201).send(user)
+    } catch (error) {
+      res.status(400).send({ message: 'Error creating user', err }); 
+    }
   },
-  find: (req, res) => {
-    UsersService.find()
-      .then(users => res.status(200).send(users))
-      .catch(err => res.status(404).send({ message: 'Users not found', err }));
+  find: async (req, res) => {
+    try {
+      const users = await UsersService.find();
+      res.status(200).send(users)
+    } catch (error) {
+      res.status(404).send({ message: 'Users not found', err });
+    }
   },
 }
