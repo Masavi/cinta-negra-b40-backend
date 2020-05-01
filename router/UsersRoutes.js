792e00f6ec860411d1d8f1e4b81f4607a7cb1130
maui <maui@devf.mx>
2020-05-01 12:21:57 -0500

    Separamos rutas de usuario en su propio archivo y creamos manejador de rutas (index)

---
+++ router/UsersRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const Users = require('../models/Users');
+
+// CREATE
+router.post('/api/v1/users', (req, res) => {
+  Users.create(req.body)
+    .then(user => res.status(201).send(user))
+    .catch(err => res.status(400).send({ message: 'Error creating user', err }));
+});
+
+// GET (ALL)
+router.get('/api/v1/users', (req, res) => {
+  Users.find()
+    .then(users => res.status(200).send(users))
+    .catch(err => res.status(404).send({ message: 'Users not found', err }));
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -8,23 +8,7 @@ server.use(express.json());
 // Endpoints
 server.get('/', (req, res) => res.send('Hello World!'));
 
-const Users = require('../models/Users');
-
-// CRUD Usuario
-
-// CREATE
-server.post('/api/v1/users', (req, res) => {
-  Users.create(req.body)
-    .then(user => res.status(201).send(user))
-    .catch(err => res.status(400).send({ message: 'Error creating user', err }));
-});
-
-// GET (ALL)
-server.get('/api/v1/users', (req, res) => {
-  Users.find()
-    .then(users => res.status(200).send(users))
-    .catch(err => res.status(404).send({ message: 'Users not found', err }));
-});
+server.use(require('../router'));
 
 // exportar server para poder requerirlo desde otros archivos
 module.exports = { server, PORT };
