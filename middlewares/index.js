86736b30c2d500a67601de0163138d28872e684f
maui <maui@devf.mx>
2020-05-05 20:15:11 -0500

    Refactorizar verificacion de jwt a carpeta de middlewares

---
+++ middlewares/index.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+  verifyToken: (req, res, next) => {
+    try {
+      const { authorization } = req.headers;
+      const token = authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.decoded = decoded;
+      next(); 
+    } catch (error) {
+      res.status(403).send({ error })
+    }
+  },
+};
