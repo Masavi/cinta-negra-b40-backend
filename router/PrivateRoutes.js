6e9a95a516f0264591c4e6a9cca72bf488110de4
maui <maui@devf.mx>
2020-05-06 21:02:18 -0500

    Agregar rutas de roles

--- router/PrivateRoutes.js
+++ router/PrivateRoutes.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./UsersRoutes'));
+router.use(require('./RolesRoutes'));
 
 module.exports = router;
---
+++ router/RolesRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { RolesController } = require('../controller');
+const { RolesValidator } = require('../validators')
+
+router.post('/users/:id/roles', RolesValidator.create, RolesController.create);
+// router.get('/users/:id/roles', RolesController.find);
+// router.patch('/users/:id/roles/:idRole', RolesValidator.update, RolesController.findByIdAndUpdate);
+// router.delete('/users/:id/roles/:idRole', RolesController.findByIdAndDelete);
+
+module.exports = router;
