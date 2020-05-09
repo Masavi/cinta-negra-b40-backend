f6f7b70450bb0a2b92c822d3a5c8b4298ef43add
maui <maui@devf.mx>
2020-05-08 20:33:32 -0500

    Agregar middleware para manejar archivos en req.files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const server = express();
+const fileUpload = require('express-fileupload');
 const PORT = process.env.PORT || 3000;
 const { errors } = require('celebrate');
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+server.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir : '/tmp/',
+}));
 
 // Endpoints
 server.get('/', (req, res) => res.send('Hello World!'));
